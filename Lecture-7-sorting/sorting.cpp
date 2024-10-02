f110891b83f5cc9d20e543b66c591113f7d46d48
Manish Dolui <138279568+Manish453400000@users.noreply.github.com>
2024-10-02 13:47:55 +0530

    Oct 2 - quick sort

--- Lecture-7-sorting/sorting.cpp
+++ Lecture-7-sorting/sorting.cpp
@@ -52,48 +52,94 @@ void insertion_sort(int arr[], int n)
     }
   }
 }
-void merge_sort(vector<int> arr, int low, int high)
+void merge(vector<int> &arr, int low, int mid, int high)
 {
-  if (low >= high)
+  vector<int> temp;
+  int left = low;
+  int right = mid + 1;
+  while (left <= mid && right <= high)
+  {
+    if (arr[left] <= arr[right])
+    {
+      temp.push_back(arr[left]);
+      left++;
+    }
+    else
+    {
+      temp.push_back(arr[right]);
+      right++;
+    }
+  }
+  while (left <= mid)
+  {
+    temp.push_back(arr[left]);
+    left++;
+  }
+  while (right <= high)
+  {
+    temp.push_back(arr[right]);
+    right++;
+  }
+  for (int i = low; i <= high; i++)
+  {
+    arr[i] = temp[i - low];
+  }
+}
+void merge_sort(vector<int> &arr, int x, int y)
+{
+  int low = x;
+  int high = y;
+  if (low == high)
     return;
   int mid = (low + high) / 2;
   merge_sort(arr, low, mid);
   merge_sort(arr, mid + 1, high);
-
-  vector<int> temp;
-  int i = low;
-  int j = mid + 1;
-  while (i <= mid && j <= high)
-  {
-    if (arr[low] < arr[mid])
-    {
-      temp.push_back(arr[i]);
-      i++;
-    }
-    else
-    {
-      temp.push_back(arr[j]);
-      j++;
-    }
-  }
-  for (auto it : temp)
-  {
-    cout << it << " ";
-  }
-  cout << endl;
+  merge(arr, low, mid, high);
 }
 
+int partition(vector<int> &arr, int low, int high)
+{
+  int pivot = arr[low];
+  int i = low;
+  int j = high;
+  while (i < j)
+  {
+    while (arr[i] <= pivot && i <= high - 1)
+    {
+      i++;
+    }
+    while (arr[j] > pivot && j >= low - 1)
+    {
+      j--;
+    }
+    if (i < j)
+      swap(arr[i], arr[j]);
+  }
+  swap(arr[low], arr[j]);
+  return j;
+}
+void quick_sort(vector<int> &arr, int low, int high)
+{
+  if (low >= high)
+    return;
+  int pIndex = partition(arr, low, high);
+  quick_sort(arr, low, pIndex - 1);
+  quick_sort(arr, pIndex + 1, high);
+}
 //
 //
 int main()
 {
   int n = 10;
-  vector<int> arr = {2, 45, 36, 23, 5, 3, 44, 12, 1, 5};
+  vector<int> arr = {5, 4, 6, 2, 1, 8, 7, 9, 3};
+  int low = 0;
+  int high = arr.size() - 1;
 
   // selection_sort(arr, n);
   // bubble_sort(arr, n);
   // insertion_sort(arr, n);
-  merge_sort(arr, 0, arr.size() - 1);
+  // merge_sort(arr, low, high); // tc --> O(nlog2n) sc --> O(n)
+  quick_sort(arr, low, high);
 
   // print -----------------
   for (auto it : arr)
