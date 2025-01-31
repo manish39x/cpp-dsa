7a9a5955ecd000b2c4daa57606139cecd2891fb3
Manish Dolui <doluibimal908@gmail.com>
2025-01-31 10:23:42 +0530

    chapter 1 basics

---
+++ cpp_training/chapter-1/README.md
@@ -0,0 +1,14 @@
+## TOPICS
+
+1. Errors in c++ program
+   .runtime errors
+   .compile time errors
+   .warnings
+2. DataType
+   .int
+   .float
+   .double
+   .bool
+   .char
+   .void
+   .auto
---
+++ cpp_training/chapter-1/basics.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// make a sqrt function
+int sqrt(float n)
+{
+  float x = pow(n, 0.5);
+  return int(x);
+}
+
+int main()
+{
+  int x = 22222;
+  cout << sizeof(x) << endl;
+
+  return 0;
+}
+/* CODE'S 0 & 1 FORM[BINARY, DECIMAL, OCTAL, HEXADECIMAL]
+  int number1 = 15;         // Decimal
+  int number2 = 017;        // Octal
+  int number3 = 0x0F;       // Hexadecimal
+  int number4 = 0b00001111; // Binary
+
+  cout << "number1 : " << number1 << endl;
+  cout << "number2 : " << number2 << endl;
+  cout << "number3 : " << number3 << endl;
+  cout << "number4 : " << number4 << endl;
+*/
+
+/* INPUT OUTPUT
+ cout << "Hello c++20" << endl;
+  int age;
+  string name;
+
+  int x;
+  cout << "x = ";
+  cin >> x;
+  cin.ignore();
+
+  cout << "Enter your name: ";
+  getline(cin, name);
+
+  cout << "Enter Your age: ";
+  cin >> age;
+
+  cout << "Hello " << name << " you are " << age << " year old" << endl;
+  // cerr << "Error Message: Something went wrong" << endl;
+  // clog << "Log Message: Somthing Happened" << endl;
+*/
